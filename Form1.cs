1ab1ccf146ff19403a729b049fd5b5e90d95d5f5
HENRY ANDERSON SANCHEZ CORTEZ <USSS030722@ugb.edu.sv>
2022-08-21 13:51:07 -0600

    Agregar un objeto aleatorio y una lista de iconos

--- Form1.cs
+++ Form1.cs
@@ -9,7 +9,21 @@ using System.Windows.Forms;
 namespace semana_4_proyecto_de_juego_de_parejas_henry_sanchez
 {
     public partial class Form1 : Form
+
     {
+        
+        Random random = new Random();
+
+        
+        List<string> icons = new List<string>()
+      {
+        "!", "!", "N", "N", ",", ",", "k", "k",
+        "b", "b", "v", "v", "w", "w", "z", "z"
+      };
+
+
+
+
         public Form1()
         {
             InitializeComponent();
